b40856b216007d759fcca20219ee01882ef895cc
Bartosz Gniado <gniadobartosz@gmail.com>
2012-10-18 11:43:07 +0200

    dodanie fortunki, aktualizacja readme

---
+++ FORTUNKA/app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = '38f5c11ee29d0c26a90aed7fcf4f70f074b507983912cbb647624acc86aa007ff3175ec22cab9425d2694854fce5cb23e6e87af88e84f6f65f977126e63688fb'
---
+++ README.md
@@ -0,0 +1,10 @@
+ASI
+======
+
+18.10.2012: Dodanie fortunki do repozytorium.
+
+17.10.2012: Jestem w trakcie rewolucji systemowych na moim sprzęcie. W ciągu kilku dni zacznę poważniej pracować nad fortunką.
+
+16.10.2012: Uporządkowałem i utworzyłem na nowo repozytorium, w którym dokumentować będę moją pracę.
+
+
